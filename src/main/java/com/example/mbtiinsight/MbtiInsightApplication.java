20eb432bfccbd5c9e73564bfee59d877ef66017d
MinjeMun <mmj010220@gmail.com>
2023-06-30 03:18:56 +0900

    Initial commit

---
+++ src/main/java/com/example/mbtiinsight/MbtiInsightApplication.java
@@ -0,0 +1,13 @@
+package com.example.mbtiinsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbtiInsightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbtiInsightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mbtiinsight/MbtiInsightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mbtiinsight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbtiInsightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
